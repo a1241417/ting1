0ddb7c313b1831966cf37bda128df050ceb251fc
a1241417 <a1241417@gamil.com>
2020-03-11 21:42:29 +0800

    bookmain

---
+++ bookmain.java
@@ -0,0 +1,7 @@
+public class bookmain{
+    public static void main(String[]args){
+        book book1 = new book();
+        book1.setbook("cindy","cat","120","ting","cat story");
+        book1.getbook();
+    }
+}
