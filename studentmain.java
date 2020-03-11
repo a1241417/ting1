5f2acc61eb6a251cbe10fe22807d03a75bb77c55
a1241417 <a1241417@gamil.com>
2020-03-11 22:09:16 +0800

    studentmain

---
+++ studentmain.java
@@ -0,0 +1,7 @@
+public class studentmain{
+    public static void main(String[]args){
+        student student1 = new student();
+        student1.setstudent("ting","68","180","chiayi","handsome");
+        student1.getstudent();
+    }
+}
